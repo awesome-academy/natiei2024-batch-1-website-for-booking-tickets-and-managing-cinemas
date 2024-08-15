dcd4af7cf120b686ac64dc1a8f19fc930917cf2a
thucvijul07 <108745183+thucvijul07@users.noreply.github.com>
2024-08-15 23:51:51 +0700

    Task.78914

--- src/main/java/cinemas/controllers/user/MoviesController.java
+++ src/main/java/cinemas/controllers/user/MoviesController.java
@@ -1,24 +1,33 @@
 package cinemas.controllers.user;
 
 import cinemas.models.Movie;
+import cinemas.enums.MovieStatus;
 import cinemas.services.MoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/movies")
 public class MoviesController {
     @Autowired
     private MoviesService moviesService;
-
-    @GetMapping("/movies/{id}")
+    @GetMapping("/now-showing")
+    public String getShowingMovie(Model model) {
+        List<Movie> movies = moviesService.getMoviesByStatus(MovieStatus.NOW_SHOWING);
+        model.addAttribute("movies", movies);
+        return "user/movies/now-showing";
+    }
+    @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         Movie movie = moviesService.findById(id).orElseThrow(() -> new RuntimeException("Movie not found"));
         model.addAttribute("movie", movie);
         return "user/movies/show";
     }
+
 }
